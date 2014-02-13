304890ffcc322e0d5e5fa23d7c38905e2f6672ea
Fedor Kozlov <kozlovfedor@gmail.com>
2014-02-13 23:22:43 +0400

    [+] basic publication parsing

--- CeurWsParser/spider.py
+++ CeurWsParser/spider.py
@@ -5,12 +5,29 @@ from grab.spider import Spider, Task, Data
 from grab.tools.logs import default_logging
 from grab import Grab
 
+def parse_workshop_publication( workshop, node):
+    link = node.find("a")
+    publication_link = workshop+link.get('href')
+    publication_label = link.text
+    br_tag = node.find("br")
+    i_tag = node.find("i")
+    if not i_tag is None:
+        publication_authors = i_tag.text.split(',')
+    else:
+        publication_authors = br_tag.tail.split(',')
+
+    print "**** FIND NEW PAPER *********"
+    print publication_label.strip()
+    print publication_link.strip()
+    for publication_author in publication_authors:
+        print publication_author.strip()
+
 def format_str(text):
         text=text.encode('utf8')
         text=' '.join(text.split())
         return text
 
-class KinoSpider(Spider):
+class CEURSpider(Spider):
 
     def task_initial(self, grab, task):
         #for xpath in grab.tree.xpath('//*/table/tbody/tr/td/b/font/a/@href'):
@@ -27,16 +44,79 @@ class KinoSpider(Spider):
                 url = xpath.encode('utf8')
                 #yield Task('workshop', url = url)
 
+
+
     def task_workshop(self, grab, task):
         print "Parsing workshop " + task.url + "..."
 
         self.out.write("<" + task.url + ">" + " <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://swrc.ontoware.org/ontology#Proceedings> .\n")
-        
-        #workshop_vol_acronym=format_str(grab.tree.xpath('//*/span[@class="CEURVOLACRONYM"]/text()')[0])
+
+        # parse workshops
+        # vol-1
+        for node in grab.tree.xpath('//*/ul/li | //*/ol/li'):
+            parse_workshop_publication(task.url, node)
         #print workshop_vol_acronym
 
+
+
 def main():
-    initial_urls = ["http://ceur-ws.org/", "http://ceur-ws.org/Vol-1/", "http://ceur-ws.org/Vol-315/paper8.pdf"]
+    initial_urls = ["http://ceur-ws.org/Vol-1"]
+    full_initial_urls = ["http://ceur-ws.org/",
+                    "http://ceur-ws.org/Vol-315/paper8.pdf",
+                    "http://ceur-ws.org/Vol-1085/",
+                    "http://ceur-ws.org/Vol-1081/",
+                    "http://ceur-ws.org/Vol-1044/",
+                    "http://ceur-ws.org/Vol-1019/",
+                    "http://ceur-ws.org/Vol-1008/",
+                    "http://ceur-ws.org/Vol-996/",
+                    "http://ceur-ws.org/Vol-994/",
+                    "http://ceur-ws.org/Vol-981/",
+                    "http://ceur-ws.org/Vol-979/",
+                    "http://ceur-ws.org/Vol-951/",
+                    "http://ceur-ws.org/Vol-946/",
+                    "http://ceur-ws.org/Vol-943/",
+                    "http://ceur-ws.org/Vol-937/",
+                    "http://ceur-ws.org/Vol-936/",
+                    "http://ceur-ws.org/Vol-930/",
+                    "http://ceur-ws.org/Vol-929/",
+                    "http://ceur-ws.org/Vol-919/",
+                    "http://ceur-ws.org/Vol-914/",
+                    "http://ceur-ws.org/Vol-906/",
+                    "http://ceur-ws.org/Vol-905/",
+                    "http://ceur-ws.org/Vol-904/",
+                    "http://ceur-ws.org/Vol-903/",
+                    "http://ceur-ws.org/Vol-902/",
+                    "http://ceur-ws.org/Vol-901/",
+                    "http://ceur-ws.org/Vol-900/",
+                    "http://ceur-ws.org/Vol-895/",
+                    "http://ceur-ws.org/Vol-890/",
+                    "http://ceur-ws.org/Vol-875/",
+                    "http://ceur-ws.org/Vol-869/",
+                    "http://ceur-ws.org/Vol-859/",
+                    "http://ceur-ws.org/Vol-840/",
+                    "http://ceur-ws.org/Vol-839/",
+                    "http://ceur-ws.org/Vol-838/",
+                    "http://ceur-ws.org/Vol-814/",
+                    "http://ceur-ws.org/Vol-813/",
+                    "http://ceur-ws.org/Vol-800/",
+                    "http://ceur-ws.org/Vol-798/",
+                    "http://ceur-ws.org/Vol-784/",
+                    "http://ceur-ws.org/Vol-783/",
+                    "http://ceur-ws.org/Vol-782/",
+                    "http://ceur-ws.org/Vol-779/",
+                    "http://ceur-ws.org/Vol-778/",
+                    "http://ceur-ws.org/Vol-775/",
+                    "http://ceur-ws.org/Vol-736/",
+                    "http://ceur-ws.org/Vol-721/",
+                    "http://ceur-ws.org/Vol-671/",
+                    "http://ceur-ws.org/Vol-669/",
+                    "http://ceur-ws.org/Vol-658/",
+                    "http://ceur-ws.org/Vol-628/",
+                    "http://ceur-ws.org/Vol-538/",
+                    "http://ceur-ws.org/Vol-369/",
+                    "http://ceur-ws.org/Vol-315/",
+                    "http://ceur-ws.org/Vol-232/",
+                    "http://ceur-ws.org/Vol-1/"]
 
     threads = 5
     default_logging(grab_log="log.txt")
@@ -44,7 +124,7 @@ def main():
     fl = open("out.txt","w")
     flval = open("outval.txt","w")
 
-    bot = KinoSpider(thread_number=threads, network_try_limit = 2)
+    bot = CEURSpider(thread_number=threads, network_try_limit = 2)
     bot.initial_urls = initial_urls
     bot.out = fl
     bot.validate = flval;
