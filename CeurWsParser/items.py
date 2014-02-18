5bba2d4fb4f4b9503f18ed7681046389092c0ded
Maxim Kolchin <kolchinmax@gmail.com>
2014-02-19 01:53:47 +0400

    Replaced .addN() to .add() back

--- CeurWsParser/items.py
+++ CeurWsParser/items.py
@@ -55,14 +55,12 @@ class Publication(Model):
     def save(self, graph):
         proceedings = URIRef(config.id['proceedings'] + self.volume_number)
         publication = URIRef(config.id['publication'] + urllib.quote('ceus-ws-' + self.volume_number + '-' + self.title))
-        graph.addN([
-                    (proceedings, DCTERMS.hasPart, publication, ''),
-                    (publication, RDF.type, FOAF.Document, ''),
-                    (publication, DCTERMS.partOf, proceedings, ''),
-                    (publication, RDF.type, SWRC.InProceedings, ''),
-                    (publication, RDFS.label, Literal(self.title, datatype=XSD.string), ''),
-                    (publication, FOAF.homepage, Literal(self.link, datatype=XSD.anyURI), '')
-                ])
+        graph.add((proceedings, DCTERMS.hasPart, publication))
+        graph.add((publication, RDF.type, FOAF.Document))
+        graph.add((publication, DCTERMS.partOf, proceedings))
+        graph.add((publication, RDF.type, SWRC.InProceedings))
+        graph.add((publication, RDFS.label, Literal(self.title, datatype=XSD.string)))
+        graph.add((publication, FOAF.homepage, Literal(self.link, datatype=XSD.anyURI)))
         for creator in self.creators:
             agent = creator.save(graph)
             graph.add((agent, DC.creator, publication))
@@ -77,30 +75,25 @@ class Workshop(Model):
         from datetime import datetime
         workshop = URIRef(config.id['workshop'] + self.proceedings.volume_number)
         proceedings = URIRef(config.id['proceedings'] + self.proceedings.volume_number)
-        graph.addN([
-                    (workshop, RDF.type, BIBO.Workshop, ''),
-                    (workshop, RDFS.label, Literal(self.label, datatype=XSD.string), ''),
-                    (proceedings, BIBO.presentedAt, workshop, '')
-                ])
+        graph.add((workshop, RDF.type, BIBO.Workshop))
+        graph.add((workshop, RDFS.label, Literal(self.label, datatype=XSD.string)))
+        graph.add((proceedings, BIBO.presentedAt, workshop))
         if isinstance(self.time, list) and len(self.time) > 0:
-            graph.addN([
-                        (workshop, TIMELINE.beginsAtDateTime, Literal(self.time[0].strftime('%Y-%m-%d'), datatype=XSD.date), ''),
-                        (workshop, TIMELINE.endsAtDateTime, Literal(self.time[1].strftime('%Y-%m-%d'), datatype=XSD.date), '')
-                    ])
+            graph.add((workshop, TIMELINE.beginsAtDateTime, Literal(self.time[0].strftime('%Y-%m-%d'), datatype=XSD.date)))
+            graph.add((workshop, TIMELINE.endsAtDateTime, Literal(self.time[1].strftime('%Y-%m-%d'), datatype=XSD.date)))
         elif isinstance(self.time, datetime):
             graph.add((workshop, TIMELINE.atDate, Literal(self.time.strftime('%Y-%m-%d'), datatype=XSD.date)))
 
 class Proceedings(Model):
     def save(self, graph):
         proceedings = URIRef(config.id['proceedings'] + self.volume_number)
-        graph.addN([(proceedings, RDF.type, SWRC.Proceedings, ''),
-                    (proceedings, RDFS.label, Literal(self.label, datatype = XSD.string), ''),
-                    (proceedings, FOAF.homepage, Literal(self.url, datatype = XSD.anyURI), ''),
-                    (proceedings, DCTERMS.issued, Literal(self.submittion_date.strftime('%Y-%m-%d'), datatype=XSD.date), '')])
+        graph.add((proceedings, RDF.type, SWRC.Proceedings))
+        graph.add((proceedings, RDFS.label, Literal(self.label, datatype = XSD.string)))
+        graph.add((proceedings, FOAF.homepage, Literal(self.url, datatype = XSD.anyURI)))
+        graph.add((proceedings, DCTERMS.issued, Literal(self.submittion_date.strftime('%Y-%m-%d'), datatype=XSD.date)))
         for editor in self.editors:
             agent = URIRef(config.id['person'] + urllib.quote(editor.encode('utf-8')))
-            graph.addN([(agent, RDF.type, FOAF.Agent, ''), 
-                        (agent, FOAF.name, Literal(editor, datatype = XSD.string), ''),
-                        (proceedings, SWRC.editor, agent, ''), 
-                        (agent, DC.creator, proceedings, '')
-                    ])
+            graph.add((agent, RDF.type, FOAF.Agent))
+            graph.add((agent, FOAF.name, Literal(editor, datatype = XSD.string)))
+            graph.add((proceedings, SWRC.editor, agent))
+            graph.add((agent, DC.creator, proceedings))
