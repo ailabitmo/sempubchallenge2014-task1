77daae121284e80657c9697bbda031ed419ad5ac
Maxim Kolchin <kolchinmax@gmail.com>
2014-02-20 01:43:16 +0400

    Extracted workshop parser to a seperate file

--- CeurWsParser/items.py
+++ CeurWsParser/items.py
@@ -1,42 +1,46 @@
-import config
 import urllib
-from rdflib import URIRef, Literal, Namespace, BNode
+
+from rdflib import URIRef, Literal, Namespace
 from rdflib.namespace import RDF, RDFS, FOAF, DC, DCTERMS, XSD
+
+import config
+
 
 SWRC = Namespace("http://swrc.ontoware.org/ontology#")
 EVENT = Namespace("http://purl.org/NET/c4dm/event.owl#")
 TIMELINE = Namespace("http://purl.org/NET/c4dm/timeline.owl#")
 BIBO = Namespace("http://purl.org/ontology/bibo/")
 
-class Model:
 
+class Model:
     def __setattr__(self, name, value):
         self.__dict__[name] = self.trim(value)
-    
+
     def trim(self, obj):
         import re
-        if isinstance(obj, str) or isinstance(obj, unicode):
+
+        if obj is str or obj is unicode:
             return re.sub(r'\s{2,}|\n', '', obj)
-        elif isinstance(obj, list) and len(obj) > 0 and (isinstance(obj[0], str) or isinstance(obj[0], unicode)):
+        elif obj is list and len(obj) > 0 and (obj[0] is str or obj[0] is unicode):
             for i in range(0, len(obj)):
                 obj[i] = self.trim(obj[i])
             return obj
         else:
             return obj
 
+
 class Creator(Model):
     def __init__(self, name=None):
         self.name = name
 
     def pretty_print(self):
-        print "EDITOR NAME: ",self.name
+        print "EDITOR NAME: ", self.name
 
     def save(self, graph):
         agent = URIRef(config.id['person'] + urllib.quote(self.name.encode('utf-8')))
         graph.add((agent, RDF.type, FOAF.Agent))
-        graph.add((agent, FOAF.name, Literal(self.name, datatype = XSD.string)))
+        graph.add((agent, FOAF.name, Literal(self.name, datatype=XSD.string)))
         return agent
-
 
 
 class Publication(Model):
@@ -54,7 +58,8 @@ class Publication(Model):
 
     def save(self, graph):
         proceedings = URIRef(config.id['proceedings'] + self.volume_number)
-        publication = URIRef(config.id['publication'] + urllib.quote('ceus-ws-' + self.volume_number + '-' + self.title))
+        publication = URIRef(
+            config.id['publication'] + urllib.quote('ceus-ws-' + self.volume_number + '-' + self.title))
         graph.add((proceedings, DCTERMS.hasPart, publication))
         graph.add((publication, RDF.type, FOAF.Document))
         graph.add((publication, DCTERMS.partOf, proceedings))
@@ -68,32 +73,40 @@ class Publication(Model):
 
 
 class Workshop(Model):
-    def __init__(self):
+    def __init__(self, volume_number):
         self.time = []
+        self.volume_number = volume_number
+        self.uri = URIRef(config.id['workshop'] + self.volume_number)
 
     def save(self, graph):
         from datetime import datetime
-        workshop = URIRef(config.id['workshop'] + self.proceedings.volume_number)
-        proceedings = URIRef(config.id['proceedings'] + self.proceedings.volume_number)
-        graph.add((workshop, RDF.type, BIBO.Workshop))
-        graph.add((workshop, RDFS.label, Literal(self.label, datatype=XSD.string)))
-        graph.add((proceedings, BIBO.presentedAt, workshop))
+
+        proceedings = URIRef(config.id['proceedings'] + self.volume_number)
+        graph.add((self.uri, RDF.type, BIBO.Workshop))
+        graph.add((self.uri, RDFS.label, Literal(self.label, datatype=XSD.string)))
+        graph.add((proceedings, BIBO.presentedAt, self.uri))
         if isinstance(self.time, list) and len(self.time) > 0:
-            graph.add((workshop, TIMELINE.beginsAtDateTime, Literal(self.time[0].strftime('%Y-%m-%d'), datatype=XSD.date)))
-            graph.add((workshop, TIMELINE.endsAtDateTime, Literal(self.time[1].strftime('%Y-%m-%d'), datatype=XSD.date)))
+            graph.add(
+                (self.uri, TIMELINE.beginsAtDateTime, Literal(self.time[0].strftime('%Y-%m-%d'), datatype=XSD.date)))
+            graph.add(
+                (self.uri, TIMELINE.endsAtDateTime, Literal(self.time[1].strftime('%Y-%m-%d'), datatype=XSD.date)))
         elif isinstance(self.time, datetime):
-            graph.add((workshop, TIMELINE.atDate, Literal(self.time.strftime('%Y-%m-%d'), datatype=XSD.date)))
+            graph.add((self.uri, TIMELINE.atDate, Literal(self.time.strftime('%Y-%m-%d'), datatype=XSD.date)))
+
 
 class Proceedings(Model):
+    def __init__(self, volume_number):
+        self.volume_number = volume_number
+        self.uri = URIRef(config.id['proceedings'] + self.volume_number)
+
     def save(self, graph):
-        proceedings = URIRef(config.id['proceedings'] + self.volume_number)
-        graph.add((proceedings, RDF.type, SWRC.Proceedings))
-        graph.add((proceedings, RDFS.label, Literal(self.label, datatype = XSD.string)))
-        graph.add((proceedings, FOAF.homepage, Literal(self.url, datatype = XSD.anyURI)))
-        graph.add((proceedings, DCTERMS.issued, Literal(self.submittion_date.strftime('%Y-%m-%d'), datatype=XSD.date)))
+        graph.add((self.uri, RDF.type, SWRC.Proceedings))
+        graph.add((self.uri, RDFS.label, Literal(self.label, datatype=XSD.string)))
+        graph.add((self.uri, FOAF.homepage, Literal(self.url, datatype=XSD.anyURI)))
+        graph.add((self.uri, DCTERMS.issued, Literal(self.submission_date.strftime('%Y-%m-%d'), datatype=XSD.date)))
         for editor in self.editors:
             agent = URIRef(config.id['person'] + urllib.quote(editor.encode('utf-8')))
             graph.add((agent, RDF.type, FOAF.Agent))
-            graph.add((agent, FOAF.name, Literal(editor, datatype = XSD.string)))
-            graph.add((proceedings, SWRC.editor, agent))
-            graph.add((agent, DC.creator, proceedings))
+            graph.add((agent, FOAF.name, Literal(editor, datatype=XSD.string)))
+            graph.add((self.uri, SWRC.editor, agent))
+            graph.add((agent, DC.creator, self.uri))
--- CeurWsParser/spider.py
+++ CeurWsParser/spider.py
@@ -1,113 +1,51 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from grab.spider import Spider, Task, Data
-from grab.tools.logs import default_logging
-from grab import Grab
-from items import Publication, Proceedings, Workshop
 import re
+import traceback
 
-import publication_parser
-
+from grab.spider import Spider, Task
+from grab.tools.logs import default_logging
+from grab.error import DataNotFound
 import rdflib
-from rdflib.namespace import FOAF, RDF, RDFS, XSD, DC, DCTERMS
-import urllib
-import config
 from rdflib.plugins.stores import sparqlstore
 
-def parse_workshop_publication_mf(repo, workshop, link):
-    publication = Publication()
-    publication.link = workshop + link.get('href')
-    publication.title = link.find_class('CEURTITLE')[0].text
-    publication.volume_number = re.match(r'.*http://ceur-ws.org/Vol-(\d+).*', workshop).group(1)
-
-    print "Pub link:" + publication.link
-    print "Pub label: " + publication.title
-
-    publication.save(repo)
+from items import Proceedings
+import publication_parser
+import workshop_parser
+import config
 
 
-def parse_workshop_publication(repo, workshop, link):
-    publication = Publication()
-    publication.link = workshop + link.get('href')
-    publication.title = link.text.strip()
-    publication.volume_number = re.match(r'.*http://ceur-ws.org/Vol-(\d+).*', workshop).group(1)
-    publication.save(repo)
-
-def format_str(text):
-    text = text.encode('utf8')
-    text=' '.join(text.split())
-    return text
-
-def parse_workshop_summary(repo, tr):
-    link = tr[0].find('.//td[last()]//a[@href]') #link(<a>) to workshop
-    if link.get('href') not in config.input_urls:
-        return
-    workshop = Workshop()
-    proceedings = Proceedings()
-    workshop.label = link.text
-    workshop.url = link.get('href')
-    workshop.proceedings = proceedings
-    proceedings.workshop = workshop
+def parse_proceedings_summary(obj):
+    link = obj[0].find('.//td[last()]//a[@href]')
+    volume_number = re.match(r'.*http://ceur-ws.org/Vol-(\d+).*', link.get('href')).group(1)
+    proceedings = Proceedings(volume_number)
     proceedings.url = link.get('href')
-    proceedings.volume_number = re.match(r'.*http://ceur-ws.org/Vol-(\d+).*', proceedings.url).group(1)
-    summary = tr[1].find('.//td[last()]').text_content() #desription of workshop (title, editors, etc.)
-    summary_match = re.match(r"(.*)(\nEdited\s*by\s*:\s*)(.*)(\nSubmitted\s*by\s*:\s*)(.*)(\nPublished\s*on\s*CEUR-WS:\s*)(.*)(\nONLINE)(.*)", 
-                    summary, re.I | re.M | re.S)
+    summary_match = re.match(
+        r'(.*)(\nEdited\s*by\s*:\s*)(.*)(\nSubmitted\s*by\s*:\s*)(.*)(\nPublished\s*on\s*CEUR-WS:\s*)(.*)(\nONLINE)(.*)',
+        obj[1].find('.//td[last()]').text_content(), re.I | re.M | re.S)
     if summary_match:
         from datetime import datetime
-        print "Parsed summary of workshop " + proceedings.url
-        proceedings.label = re.sub(r'\n', '', summary_match.group(1)) #title of workshop
-        proceedings.editors = re.split(r",{1}\s*", summary_match.group(3)) #editors of workshop
+
+        proceedings.label = re.sub(r'\n', '', summary_match.group(1))
+        proceedings.editors = re.split(r",+\s*", summary_match.group(3))
         #submitters = m.group(5)
-        time_match_1 = re.match(r'.*,\s*([a-zA-Z]+)[,\s]*(\d{1,2})[\w\s]*,\s*(\d{4})', proceedings.label, re.I)
-        time_match_2 = re.match(r'.*,\s*([a-zA-Z]+)[,\s]*(\d{1,2})[\w\s]*-\s*(\d{1,2})[\w\s,]*(\d{4})', proceedings.label, re.I)
-        time_match_3 = re.match(r'.*,\s*([a-zA-Z]+)\s*(\d+)\s*-\s*([a-zA-Z]+)\s*(\d+)\s*,\s*(\d{4})', proceedings.label, re.I)
-        if time_match_1:
-            try:
-                workshop.time = datetime.strptime(time_match_1.group(1) + "-" + time_match_1.group(2) + "-" + time_match_1.group(3), '%B-%d-%Y')
-            except:
-                pass
-        elif time_match_2:
-            try:
-                workshop.time = [
-                                    datetime.strptime(time_match_2.group(1) + "-" + time_match_2.group(2) + "-" + time_match_2.group(4), '%B-%d-%Y'),
-                                    datetime.strptime(time_match_2.group(1) + "-" + time_match_2.group(3) + "-" + time_match_2.group(4), '%B-%d-%Y')
-                                ]
-            except:
-                pass
-        elif time_match_3:
-            try:
-                workshop.time = [
-                                    datetime.strptime(time_match_3.group(1) + "-" + time_match_3.group(2) + "-" + time_match_3.group(5), '%B-%d-%Y'),
-                                    datetime.strptime(time_match_3.group(3) + "-" + time_match_3.group(4) + "-" + time_match_3.group(5), '%B-%d-%Y')
-                                ]
-            except:
-                pass
-        else:
-            #There is no event time information
-            pass
-        proceedings.submittion_date = datetime.strptime(proceedings.trim(summary_match.group(7)), '%d-%b-%Y')
-        workshop.save(repo)
-        proceedings.save(repo)
+        proceedings.submission_date = datetime.strptime(proceedings.trim(summary_match.group(7)), '%d-%b-%Y')
+        return proceedings
     else:
         #There is no summary information for a workshop
         pass
 
+
 class CEURSpider(Spider):
-
     def prepare(self):
-        #Configure a Graph
-        store = sparqlstore.SPARQLUpdateStore(
-                    queryEndpoint = config.sparqlstore['url'] + "/repositories/" + config.sparqlstore['repository'],
-                    update_endpoint = config.sparqlstore['url'] + "/repositories/" + config.sparqlstore['repository'] + "/statements", 
-                    context_aware = False)
-        graph = rdflib.Graph(store)
-        self.repo = graph
-
-        self.publication_results_done=0
-        self.publication_results_failed=0
-
+        self.publication_results_done = 0
+        self.publication_results_failed = 0
+        self.repo = rdflib.Graph(sparqlstore.SPARQLUpdateStore(
+            queryEndpoint=config.sparqlstore['url'] + "/repositories/" + config.sparqlstore['repository'],
+            update_endpoint=config.sparqlstore['url'] + "/repositories/" +
+                            config.sparqlstore['repository'] + "/statements",
+            context_aware=False))
 
     def task_initial(self, grab, task):
         if task.url.endswith('.pdf'):
@@ -117,45 +55,58 @@ class CEURSpider(Spider):
         elif 'Vol' in task.url:
             #parse a workshop
             print "Parsing a workshop " + task.url
-            yield Task('workshop', url = task.url)
+            yield Task('workshop', url=task.url)
         else:
             #parse the index page
             print "Parsing the index page..."
             tr = grab.tree.xpath('/html/body/table[last()]/tr[td]')
             for i in range(0, len(tr), 2):
-                parse_workshop_summary(self.repo, [tr[i], tr[i+1]])
+                url = tr[i].find('.//td[last()]//a[@href]').get('href')
+                if url in config.input_urls:
+                    try:
+                        workshop = workshop_parser.parse_workshop_summary([tr[i], tr[i + 1]])
+                        workshop.save(self.repo)
+                        proceedings = parse_proceedings_summary([tr[i], tr[i + 1]])
+                        proceedings.save(self.repo)
+                    except DataNotFound as dnf:
+                        print "[WORKSHOP %s] Parsing of summary failed!" % url
+                        traceback.print_exc()
+                        pass
 
     def task_workshop(self, grab, task):
         try:
-            publication_parser.parse_publications(self,grab,task)
-            self.publication_results_done+=1
+            publication_parser.parse_publications(self, grab, task)
+            self.publication_results_done += 1
         except:
-            self.validate.write(task.url+'\n')
-            self.publication_results_failed+=1
-
+            self.validate.write(task.url + '\n')
+            self.publication_results_failed += 1
 
     def print_stats(self):
-        print "Publications done:",self.publication_results_done
-        print "Publications failed:",self.publication_results_failed
-def main():
+        print "Publications done:", self.publication_results_done
+        print "Publications failed:", self.publication_results_failed
 
+
+def main():
     threads = 1
     default_logging(grab_log="log.txt")
-    
-    fl = open("out.txt","w")
-    flval = open("outval.txt","w")
 
-    bot = CEURSpider(thread_number=threads, network_try_limit = 2)
+    fl = open("out.txt", "w")
+    flval = open("outval.txt", "w")
+
+    bot = CEURSpider(thread_number=threads, network_try_limit=2)
     bot.initial_urls = config.input_urls
     bot.out = fl
-    bot.validate = flval;
-    try: bot.run()
-    except KeyboardInterrupt: pass
+    bot.validate = flval
+    try:
+        bot.run()
+    except KeyboardInterrupt:
+        pass
     fl.close()
     flval.close()
 
     bot.print_stats()
     #print(bot.render_stats())
+
 
 if __name__ == '__main__':
     main()
