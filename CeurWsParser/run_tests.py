8b42fd6140b7c53270287ec4f3ff040ed77db1df
Maxim Kolchin <kolchinmax@gmail.com>
2014-03-18 10:46:24 +0400

    #27 Added more tests

--- CeurWsParser/run_tests.py
+++ CeurWsParser/run_tests.py
@@ -5,9 +5,9 @@ import csv
 import traceback
 from csv import Dialect
 
-from rdflib import Graph, Literal, URIRef
+from rdflib import Graph, Literal, URIRef, util
 from rdflib.plugins.stores import sparqlstore
-from rdflib.namespace import FOAF, DCTERMS, DC
+from rdflib.namespace import FOAF, DCTERMS, DC, XSD
 
 from CeurWsParser import config
 from CeurWsParser.namespaces import SWRC, BIBO, TIMELINE
@@ -27,11 +27,25 @@ def topython(graph, term):
     return term.n3(graph.namespace_manager)
 
 
+def from_n3(string):
+    term = util.from_n3(string)
+    if isinstance(term, Literal):
+        if term.datatype is None and term.language is None:
+            term = Literal(term, datatype=URIRef('xsd:string'))
+    return term
+
+
+def normalize(namespace_manager, term):
+    if isinstance(term, Literal) and term.datatype is not None:
+        return Literal(term, datatype=namespace_manager.normalizeUri(term.datatype))
+    return term
+
+
 def read_csv(utf8_data, dialect=Dialect.delimiter):
     csv_reader = csv.reader(utf8_data, dialect=dialect, quoting=csv.QUOTE_NONE)
     lines = []
     for row in csv_reader:
-        lines.append([unicode(cell, 'utf-8') for cell in row])
+        lines.append([from_n3(unicode(cell, 'utf-8')) for cell in row])
     return lines
 
 
@@ -46,61 +60,64 @@ def print_list(l):
 def main():
     number_of_passed = 0
     number_of_failed = 0
+    number_of_failed_with_exception = 0
 
     graph = Graph(
         sparqlstore.SPARQLStore(config.sparqlstore['url'] + "/repositories/" + config.sparqlstore['repository'],
                                 context_aware=False))
     graph.bind('foaf', FOAF)
+    graph.bind('xsd', XSD)
     graph.bind('swrc', SWRC)
     graph.bind('bibo', BIBO)
     graph.bind('dcterms', DCTERMS)
     graph.bind('dc', DC)
     graph.bind('timeline', TIMELINE)
 
-    print "Configured a SPARQLStore"
-
     for f in os.listdir(TESTS_ROOT_DIR):
+        print "========================="
         print "[TEST %s] Running..." % f
         try:
             passed = True
-            query = open('%s/%s/%s' % (TESTS_ROOT_DIR, f, QUERY_FILENAME)).read()
-            result = graph.query(query)
+            with open('%s/%s/%s' % (TESTS_ROOT_DIR, f, QUERY_FILENAME)) as query_file:
+                result = graph.query(query_file.read())
 
-            results = []
-            for r in result:
-                result_line = [topython(graph, x) for x in r]
-                results.append(result_line)
+                results = []
+                for r in result:
+                    result_line = [normalize(graph.namespace_manager, x) for x in r]
+                    results.append(result_line)
 
-            print "[TEST %s] Number of results: %s" % (f, len(results))
+                print "[TEST %s] Number of results: %s" % (f, len(results))
 
-            with open('%s/%s/%s' % (TESTS_ROOT_DIR, f, EXPECTED_FILENAME), 'rb') as expected_file:
-                expected = read_csv(expected_file)
-                print "[TEST %s] Number of expected results: %s" % (f, len(expected))
-                if len(results) != len(expected):
-                    passed = False
+                with open('%s/%s/%s' % (TESTS_ROOT_DIR, f, EXPECTED_FILENAME), 'rb') as expected_file:
+                    expected = read_csv(expected_file)
+                    print "[TEST %s] Number of expected results: %s" % (f, len(expected))
+                    if len(results) != len(expected):
+                        passed = False
+                    else:
+                        for row in expected:
+                            if row not in results:
+                                print "[TEST %s] [%s] not found!" % (f, ', '.join(map(repr, row)))
+                                print "[TEST %s] Query results:" % f
+                                print_list(results)
+                                passed = False
+                                break
+
+                if passed:
+                    print "[TEST %s] Passed!" % f
+                    number_of_passed += 1
                 else:
-                    for row in expected:
-                        if row not in results:
-                            print "[TEST %s] [%s] not found!" % (f, ', '.join(map(repr, row)))
-                            print "[TEST %s] Query results:" % f
-                            print_list(results)
-                            passed = False
-                            break
-
-            if passed:
-                print "[TEST %s] Passed!" % f
-                number_of_passed += 1
-            else:
-                print "[TEST %s] Failed!" % f
-                number_of_failed += 1
+                    print "[TEST %s] Failed!" % f
+                    number_of_failed += 1
 
         except:
             print "[TEST %s] Failed with exception!" % f
+            number_of_failed_with_exception += 1
             traceback.print_exc()
 
     print "\n======RESULTS======\n"
     print "Passed: %s" % number_of_passed
     print "Failed: %s" % number_of_failed
+    print "Failed with exception: %s" % number_of_failed_with_exception
 
 
 if __name__ == '__main__':
